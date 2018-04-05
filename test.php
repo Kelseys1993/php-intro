8de789c7c8d6f972e5d9f1c687bc79018a80d4e4
Kelseys1993 <kelsey.j.smith18@gmail.com>
2018-04-05 22:47:47 +1200

    first commit

---
+++ test.php
@@ -0,0 +1,74 @@
+<?php 
+// $currentdate=date("M j, Y g:i:s a");//current date and time
+// $cd = date("d/m/y");
+// echo "Hello $cd <br>";
+// $printvalue=print ("Good Afternoon, Current date is $currentdate");
+// // echo "<br>$printvalue ";
+// echo "<br>" . $printvalue . "<br>";
+// $a = 5;
+// $b = 4;
+// echo $a + $b;
+
+// echo '<br>$a';
+
+// $a = 5;
+// $b = 4;
+// $c = $a + $b;
+// $name = "John";
+// echo " The sum of a and b = $c <br>";
+// echo gettype($name) . "<br>";
+// settype($a, "float");
+// echo gettype($a). "<br>";
+// echo (int)$b . "<br>"; //getting only the integer part of the number can also do with intval($b) function
+// echo $a%$b; //modular symbol gives the remaining numbe of a division
+
+// $p1 = 35;
+// $p2 = 47;
+// $p3 = 28;
+
+// if ($p1 > $per2) {
+//     echo "person one is older than person two";
+// } else {
+//     echo "person two is older than person one";
+// }
+// if ($p1 > $p2 && $p1 > $p2) {
+//     echo "Person One is the oldest at age $p1";
+// } else if ($p2 > $p1 && $p2 > $p3) {
+//     echo "Person Two is the oldest at age $p2";
+// } else {
+//     echo "Person Three is the oldest at age $p3";
+// }
+
+// //Palindrome checking
+// $a = "madam";
+// $b = strrev($a);
+
+// if ($a == $b) {
+//     echo "$a is a palindrome <br>";
+//     echo var_dump($a);
+// }else {
+//     echo "$a is not palindrome";
+
+// }
+// echo "<br>";
+$mark = [72,84,56,67,92,77,62,50];
+for ($i=1; $i<=2; $i++){
+    for($j=1; $j <=4; $j++){
+        echo "$i $j";
+    }
+    echo "<br>";
+}
+
+
+// for ($i=0; $i < 5; $i++) { 
+//     for ($j=0; $j <= $i; $j++) { 
+//         echo "*\t";
+//     }
+//     echo "<br>";
+// }
+
+
+
+
+
+?>
